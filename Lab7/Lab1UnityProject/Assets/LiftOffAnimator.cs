d81fe163c1f0cb7ecb38c664882056bc3d1140d9
m3hrara <mehrarasb@gmail.com>
2022-03-26 05:17:55 -0400

    Added animations

---
+++ Lab7/Lab1UnityProject/Assets/LiftOffAnimator.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LiftOffAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+}
